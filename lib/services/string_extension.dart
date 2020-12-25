740d9b513aacc76637bf694389c5fc789dd30bcd
blayziken <kingtoluwalope@gmail.com>
2020-12-25 17:08:39 +0100

    A string title case implementation

---
+++ lib/services/string_extension.dart
@@ -0,0 +1,11 @@
+extension StringExtension on String {
+  String capitalize() {
+    return "${this[0].toUpperCase()}${this.substring(1)}";
+  }
+}
+
+//https://medium.com/@sergio13prez/fetching-them-all-poke-api-62ca580981a2
+//https://pokedevs.gitbook.io/pokedex/libraries
+//https://pokeapi.co/api/v2/pokemon/blaziken
+//https://pokeapi.glitch.me/v1/pokemon/blaziken
+//https://www.google.com/search?q=pokemons&oq=pokemons&aqs=chrome..69i57j0i10l2j46i10i433j46i10j46i10i395l3.1437j1j7&sourceid=chrome&ie=UTF-8
