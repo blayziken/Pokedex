96b789e17ea6553154f7bdad50eb3d2d5e363749
blayziken <kingtoluwalope@gmail.com>
2020-12-25 23:49:29 +0100

    new test dart file

---
+++ lib/tests/zzz.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../services/NetworkAPI.dart';
+import '../Screens/home.dart';
+
+class APIFunctions {
+  APIFunctions(this.name);
+  String name;
+
+  dynamic getData99() async {
+    NetworkAPI networkAPI =
+        NetworkAPI('https://pokeapi.co/api/v2/pokemon/$name');
+
+    var pokemonData = await networkAPI.getData();
+    print('23232');
+    return (pokemonData);
+//    print('2addf');
+  }
+
+  dynamic updateUI2() async {
+    print('Entered here222222');
+    var pokeData99 = await getData99();
+
+    //FIRST API
+    String pokeName = pokeData99['name'];
+    print('pokeName ===== $pokeName');
+    return pokeData99;
+//    setState(() {
+//      showSpinner = false;
+//    });
+  }
+}
