dfc89de02ff0cf0fcc86164bec76dce281ebd1ef
blayziken <kingtoluwalope@gmail.com>
2020-12-25 17:08:08 +0100

    Added a new API reference

--- lib/services/NetworkAPI.dart
+++ lib/services/NetworkAPI.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:flutter/material.dart';
 
 class NetworkAPI {
   NetworkAPI(this.url);
@@ -7,14 +8,35 @@ class NetworkAPI {
   final String url;
 
   Future getData() async {
+//    https://pokeapi.co/api/v2/pokemon/
     http.Response response = await http.get(url);
 
     if (response.statusCode == 200) {
       String retrievedData = response.body;
 
+      print('retrievedData gotten');
       return jsonDecode(retrievedData);
     } else {
       print(response.statusCode);
+    }
+  }
+}
+
+class NetworkPokeDevAPI {
+  //https://pokedevs.gitbook.io/pokedex/resources/pokemon
+  NetworkPokeDevAPI(this.imageURL);
+
+  String imageURL;
+
+  Future getData2() async {
+    http.Response pokeDevData = await http.get(imageURL);
+
+    String retrievedpokeDevData = pokeDevData.body;
+
+    if (pokeDevData.statusCode == 200) {
+      return jsonDecode(retrievedpokeDevData);
+    } else {
+      print(pokeDevData.statusCode);
     }
   }
 }
