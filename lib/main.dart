b00d2c2a04806ea259825c0b3f9ec3e5ac08bf9c
blayziken <kingtoluwalope@gmail.com>
2021-10-23 16:25:53 +0100

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Poke Search 💤',
       theme: ThemeData(
         primarySwatch: Colors.blue,
