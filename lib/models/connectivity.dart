aab07cde8453a61d6c9d4229049189d2aaabe4e4
blayziken <kingtoluwalope@gmail.com>
2021-10-25 12:37:42 +0100

    internet connectivity done

--- lib/models/connectivity.dart
+++ lib/models/connectivity.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 
@@ -11,18 +12,16 @@ class CheckInternetConnection {
       case ConnectivityResult.wifi:
         internetStatus = "Connected to the Internet";
         contentMessage = "Connected to the Internet";
-        showCustomDialog(internetStatus, contentMessage, context);
+        // showCustomDialog(internetStatus, contentMessage, context);
         break;
       case ConnectivityResult.mobile:
         internetStatus = "Connected to the Internet";
         contentMessage = "Connected to the Internet";
-        print('Mobileeeeeeeeeeeeeeeeee');
-        showCustomDialog(internetStatus, contentMessage, context);
+        // showCustomDialog(internetStatus, contentMessage, context);
         break;
       case ConnectivityResult.none:
-        internetStatus = "You are disconnected to the Internet. ";
+        internetStatus = "No Internet Connection!. ";
         contentMessage = "Please check your internet connection";
-        print('No Internetttttt');
         showCustomDialog(internetStatus, contentMessage, context);
         break;
       case ConnectivityResult.ethernet:
@@ -40,31 +39,23 @@ class CheckInternetConnection {
             content: Text(content),
             actions: <Widget>[
               InkWell(
-                  onTap: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text("Close"))
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Close",
+                      style: TextStyle(fontSize: 17, fontWeight: FontWeight.w700),
+                    ),
+                  ],
+                ),
+              )
             ],
+            actionsPadding: EdgeInsets.all(10),
           );
         });
   }
-
-// checkConnection2(BuildContext context) async {
-//   listener = DataConnectionChecker().onStatusChange.listen((status) {
-//     switch (status) {
-//       case DataConnectionStatus.connected:
-//         internetStatus = "Connected to the Internet";
-//         contentMessage = "Connected to the Internet";
-//         _showDialog(internetStatus, contentMessage, context);
-//         break;
-//       case DataConnectionStatus.disconnected:
-//         internetStatus = "You are disconnected to the Internet. ";
-//         contentMessage = "Please check your internet connection";
-//         _showDialog(internetStatus, contentMessage, context);
-//         break;
-//     }
-//   });
-//
-//   return await DataConnectionChecker().connectionStatus;
-// }
 }
